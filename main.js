ecd9727ed7a22e3f700798829ecb0157d8be0403
NKVharshvardhan <73694587+NKVharshvardhan@users.noreply.github.com>
2022-08-29 19:08:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+var fontx=0;
+var fonty=0;
+var rightwristx=0;
+var leftwristx=0;
+var difference=0;
+
 function setup(){
     canvas=createCanvas(550,550);
     canvas.position(560,150);
@@ -13,11 +19,24 @@ function modelloaded(){
 
 function draw(){
 background("#0000FF");
+document.getElementById("font_side").innerHTML = "Width And Height of a font will be = " + difference +"px";
+fill("#00e6ff");
+stroke("#00e6ff");
+square(font,difference)
 }
 
 function gotposes(results){
-if(results.length>0)
-{
-console.log(results);
-}
-}
+    if(results.length>0)
+    {
+    console.log(results);
+    nosex=results[0].pose.font.x;
+    nosey=results[0].pose.font.y;
+    console.log("font= "+fontx+"fonty= "+fonty);
+    
+    leftwriastx=results[0].pose.leftwrist.x;
+    rightwristx=results[0].pose.leftwrist.x;
+    difference=floor(leftwristx-rightwristx); //floor is used to remove the decimal points
+    console.log("leftwristx= "+leftwristx+"rightwristx= "+rightwristx);
+    
+    }
+    }
